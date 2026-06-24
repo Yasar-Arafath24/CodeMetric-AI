fd6714ace0f0d08d1b27cc6a793c86828ff12fab
YasarArafath03 <yasar.arafath@veyrotech.com>
2026-06-24 09:31:47 +0530

    databasse updated

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -1,20 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+from pathlib import Path
 import os
+
+env_path = Path(__file__).resolve().parent.parent.parent / ".env"
+
+load_dotenv(env_path)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+print("DATABASE_URL =", DATABASE_URL)
+
 if not DATABASE_URL:
-    raise ValueError(
-        "DATABASE_URL environment variable not found."
-    )
+    raise ValueError("DATABASE_URL not found")
 
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True,
-    connect_args={
-        "sslmode": "require"
-    }
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(
